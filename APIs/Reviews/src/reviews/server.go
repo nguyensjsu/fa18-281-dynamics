2b99ea6ec6354b7bccc22b3db10b2a0f5a5023b0
ujjvalsoni <ujjval.soni@sjsu.edu>
2018-12-02 00:08:55 -0800

    main commit.

--- APIs/Reviews/src/reviews/server.go
+++ APIs/Reviews/src/reviews/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"net/http"
 )
 
@@ -20,14 +24,51 @@ func NewServer() *negroni.Negroni {
 	return n
 }
 
+
+// MongoDB Config
+var mongodb_server = "mongodb://admin:cmpe281@52.53.82.217:27017/groupproject?authSource=admin"
+var mongodb_database = "groupproject"
+var mongodb_collection = "reviews"
+
+
 // API Routes
 func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/getReviews", getReviewsHandler(formatter)).Methods("GET")
+
 }
 
 // API Ping Handler
 func pingHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		formatter.JSON(w, http.StatusOK, struct{ Test string }{"Ping WORKS!!!"})
+	}
+}
+
+// API Gumball Machine Handler
+func getReviewsHandler(formatter *render.Render) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		session, err := mgo.Dial(mongodb_server)
+		if err != nil {
+			panic(err)
+		}
+
+		if err != nil {
+			fmt.Println("Reviews API - Unable to connect to MongoDB during read operation")
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+		//var result bson.M
+		var results []Person
+		//err = c.Find(bson.M{}).All(&result)
+		err = c.Find(bson.M{}).All(&results)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(results)
+		formatter.JSON(w, http.StatusOK, results)
 	}
 }
