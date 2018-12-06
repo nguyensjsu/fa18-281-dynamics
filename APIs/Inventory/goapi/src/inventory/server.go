a5e2e08a99660e64ff216a1e6d56e449520c5a66
prajwalv <prajwal.venkatesh@sjsu.edu>
2018-12-06 13:04:33 -0800

    Update API inventory modified

--- APIs/Inventory/goapi/src/inventory/server.go
+++ APIs/Inventory/goapi/src/inventory/server.go
@@ -15,9 +15,9 @@ import (
 
 
 // MongoDB Config
-var mongodb_server = "mongodb://admin:admin@52.9.134.184:27017/admin"
+var mongodb_server = "mongodb://admin:admin@54.153.77.225"
 var mongodb_database = "inventory"
-var mongodb_collection = "inventoryItem"
+var mongodb_collection = "InventoryItem"
 
 func NewServer() *negroni.Negroni {
 	formatter := render.New(render.Options{
@@ -34,7 +34,11 @@ func NewServer() *negroni.Negroni {
 func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/inventory", getInventoryHandler(formatter)).Methods("GET")
-	mx.HandleFunc("/addInventoryItem", addItemHandler(formatter)).Methods("POST")
+	// mx.HandleFunc("/inventory", addItemToInventoryHandler(formatter)).Methods("POST")
+	// mx.HandleFunc("/inventory", updateInventoryHandler(formatter)).Methods("PUT")
+	// mx.HandleFunc("/addInventoryItem", addItemHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/inventory/delete/item_name", deleteInventoryByItem(formatter)).Methods("DELETE")
+	mx.HandleFunc("/inventory/delete", deleteInventoryHandler(formatter)).Methods("DELETE")
 }
 
 // API Ping Handler
@@ -67,12 +71,55 @@ func getInventoryHandler(formatter *render.Render) http.HandlerFunc {
 
 
 
-// API Post (Add Item to DB) Handler.
-func addItemHandler(formatter *render.Render) http.HandlerFunc {
+
+// // API item to Inventory
+// func addItemToInventoryHandler(formatter *render.Render) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, req *http.Request) {
+
+// 		decoder := json.NewDecoder(req.Body)
+// 		var i InventoryItem
+// 		err := decoder.Decode(&i)
+// 		if err != nil {
+// 			fmt.Println("Error parsing the request's body: ", err)
+// 		} 
+// 		session, err := mgo.Dial(mongodb_server)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		defer session.Close()
+// 		session.SetMode(mgo.Monotonic, true)
+// 		c := session.DB(mongodb_database).C(mongodb_collection)
+
+// 		uuid, _ := uuid.NewV4()
+// 		entry := InventoryItem{uuid.String(),
+// 				i.Item_name,
+// 				i.Item_description,
+// 				i.Item_id,
+// 				i.Item_rate,
+// 				i.Item_inventory,
+// 			}
+// 		err = c.Insert(entry)
+// 		if err != nil {
+// 			fmt.Println("Error while inserting purchase: ", err)
+// 		} else {
+// 			formatter.JSON(w, http.StatusOK, struct{ Test string }{"Item added to Inventory"})
+// 		}
+
+// 	}
+// }
+
+
+// API Delete all Inventory Items
+func deleteInventoryHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		var m inventoryItem
-		_ = json.NewDecoder(req.Body).Decode(&m)
-		fmt.Println("Item is: ", m, " ", m.name," ",m.desc," ",m.quantity," ", m.price)
+
+		decoder := json.NewDecoder(req.Body)
+		var i InventoryItem
+		err := decoder.Decode(&i)
+		if err != nil {
+			fmt.Println("Error parsing the request's body: ", err)
+		}
+
 		session, err := mgo.Dial(mongodb_server)
 		if err != nil {
 			panic(err)
@@ -80,26 +127,49 @@ func addItemHandler(formatter *render.Render) http.HandlerFunc {
 		defer session.Close()
 		session.SetMode(mgo.Monotonic, true)
 		c := session.DB(mongodb_database).C(mongodb_collection)
-		entry := inventoryItem{
-			name: m.name,
-			desc: m.desc,
-			quantity: m.quantity,
-			price: m.price,
-		}
-		err = c.Insert(entry)
+
+		_, err = c.RemoveAll(bson.M{})
 		if err != nil {
-			fmt.Println("Error while adding item to inventory: ", err)
+			formatter.JSON(w, http.StatusOK, struct{ Test string }{"No items in the inventory"})
 		} else {
-			formatter.JSON(w, http.StatusOK, struct{ Test string }{"Item added to inventory"})
+			formatter.JSON(w, http.StatusOK, struct{ Test string }{"All the items in the inventory deleted"})
 		}
 	}
 }
 
+// API Delete Inventory for a specified item
+func deleteInventoryByItem(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		decoder := json.NewDecoder(req.Body)
+		var i InventoryItem
+		err := decoder.Decode(&i)
+		if err != nil {
+			fmt.Println("Error parsing the request's body: ", err)
+		}
+
+		session, err := mgo.Dial(mongodb_server)
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+
+		err = c.Remove(bson.M{"Item_name":i.Item_name})
+		if err != nil {
+			formatter.JSON(w, http.StatusOK, struct{ Test string }{"No items in the inventory with this item name"})
+		} else {
+			formatter.JSON(w, http.StatusOK, struct{ Test string }{"Item deleted"})
+		}
+	}
+}
 
 //sample document
-// db.createCollection("inventoryItem")
+// db.createCollection("InventoryItem")
 
-// db.inventoryItem.insert({'id':1,'name':'rice','desc':'This is rice','quantity':5,'price':15})
+// db.InventoryItem.insert({'Item_id':1,'Item_name':'milk','Item_description':'This is milk','Item_inventory':5,'Item_rate':15})
+// db.InventoryItem.insert({'Item_id':1,'Item_name':'yogurt','Item_description':'This is yogurt','Item_inventory':3,'Item_rate':12})
 
 // use admin
 // db.createUser( {
@@ -109,3 +179,6 @@ func addItemHandler(formatter *render.Render) http.HandlerFunc {
 // });
 
 //const IP_MONGODB_DATABASE = 'mongodb://admin:admin@52.53.77.103:27017/admin';
+// mongo -u admin -p admin --authenticationDatabase admin
+
+
--- APIs/Inventory/goapi/src/inventory/types.go
+++ APIs/Inventory/goapi/src/inventory/types.go
@@ -1,11 +1,10 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
-
-type inventoryItem struct {
-	id       	bson.ObjectId
-	name     	string	
-	desc     	string	
-	quantity 	int 	
-	price    	int
+type InventoryItem struct {
+	Item_id 			int     `json:"item_id" bson:"item_id"`
+	Item_name     		string	`json:"item_name" bson:"item_name"`
+	Item_description 	string	`json:"item_description" bson:"item_description"`
+	Item_inventory 		int 	`json:"item_inventory" bson:"item_inventory"`
+	Item_rate    		float64 `json:"item_rate" bson:"item_rate"`
 }
+
